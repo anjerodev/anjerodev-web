52d4b8e1f79659ab39c98af99da35346dea3352d
Jepri <jepri.creations@gmail.com>
2024-10-16 10:42:53 +0700

    feat: added vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
-import icon from "astro-icon";
+import vercelStatic from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
-});
+  integrations: [tailwind(), icon()],
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
+})
